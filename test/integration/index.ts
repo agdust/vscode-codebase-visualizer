8687ca55f44462bb08457c10af23b196978370f6
Mikhail Nekrytov <mail@nekrytov.com>
2025-11-23 20:26:17 +0300

    replace glob package with native node22 glob

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -1,35 +1,30 @@
 import * as path from "path";
 import Mocha from "mocha";
-import { glob } from "glob";
+import { glob } from "fs/promises";
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
 	// Create the mocha test
 	const mocha = new Mocha({ ui: "tdd", color: true, timeout: 5 * 60 * 1000 });
 
 	const testsRoot = path.resolve(__dirname, ".");
 
-	return new Promise((c, e) => {
-		glob("**/**.test.js", { cwd: testsRoot })
-			.then((files) => {
-				// Add files to the test suite
-				files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+	for await (const f of glob("**/**.test.js", { cwd: testsRoot })) {
+		mocha.addFile(path.resolve(testsRoot, f));
+	}
 
-				try {
-					// Run the mocha test
-					mocha.run((failures) => {
-						if (failures > 0) {
-							e(new Error(`${failures} tests failed.`));
-						} else {
-							c();
-						}
-					});
-				} catch (err) {
-					console.error(err);
-					e(err);
+	return new Promise((resolve, reject) => {
+		try {
+			// Run the mocha test
+			mocha.run((failures) => {
+				if (failures > 0) {
+					reject(new Error(`${failures} tests failed.`));
+				} else {
+					resolve();
 				}
-			})
-			.catch((err) => {
-				return e(err);
 			});
+		} catch (err) {
+			console.error(err);
+			reject(err);
+		}
 	});
 }
